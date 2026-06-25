54a1366907c7aad5b556d9ec7315d5d2e0e2839a
esy385 <andresmatiascorreom@gmail.com>
2026-06-24 22:51:59 -0400

    Avance 2

--- gasto.py
+++ gasto.py
@@ -7,4 +7,22 @@ class Gasto:
     def __repr__(self):
         return f'{self.descripcion} - ${self.monto}'
     
+    def to_dict(self):
+        return {
+            'descripcion' : self.descripcion,
+            'monto' : self.monto,
+            'categoria' : self.categoria
+        }
+        
+    @classmethod
+    def from_dict(cls, datos):
+        return cls(
+            datos['descripcion'],
+            datos['monto'],
+            datos['categoria']
+        )
+        
+    
+    
+    
     
--- gestor.py
+++ gestor.py
@@ -1,5 +1,7 @@
 from gasto import Gasto
 import json
+import datetime
+import time
 
 class GestorGastos:
     def __init__(self):
@@ -27,18 +29,44 @@ class GestorGastos:
                 print(gasto)
     
     def ordernar_por_monto(self):
-        pass
+        for pasada in range(len(self._gastos)):
+            for gasto in range(len(self._gastos) - 1 - pasada):
+                if self._gastos[gasto].monto > self._gastos[gasto+1].monto:
+                    self._gastos[gasto], self._gastos[gasto+1] = self._gastos[gasto + 1], self._gastos[gasto]
+        self.mostrar_gastos()
+                
     
     def calcular_total_mensual(self):
         pass
     
     def guardar(self):
-        with open("datos.json", 'a', encoding='utf-8') as archivo:
-            pass
+        datos = []
+        
+        for gasto in self._gastos:
+            datos.append(gasto.to_dict())
+        
+        with open('datos.json', 'w', encoding='utf-8') as archivo:
+            json.dump(datos, archivo, indent=4, ensure_ascii=False)
+        
+        print('Guardando datos...')
+        time.sleep(2)
     
     def cargar(self):
-        with open("datos.json", 'r', encoding='utf-8') as archivo:
-            pass
+        try:
+            with open("datos.json", 'r', encoding='utf-8') as archivo:
+                datos = json.load(archivo)
+            
+            self._gastos = []    
+            
+            for gasto in datos:
+                self._gastos.append(Gasto.from_dict(gasto))
+        except FileNotFoundError:
+            self._gastos = []
+        
+        print('Cargando datos...')
+        time.sleep(2)
+            
+           
     
             
     
